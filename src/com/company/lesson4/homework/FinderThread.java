3edee461255fde9872da8b94a88af9ae34506fb3
Arman Ayvazyan <ayvazyan.a.arman@gmail.com>
2021-06-19 02:15:00 +0400

    lesson 3 homework

---
+++ src/com/company/lesson4/homework/FinderThread.java
@@ -0,0 +1,43 @@
+package com.company.lesson4.homework;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class FinderThread extends Thread {
+
+    private long size = 0;
+    private int count = 0;
+    private String path;
+
+
+    public FinderThread(Path path, String name) {
+        this.path = path.toString();
+        this.setName(name);
+    }
+
+    @Override
+    public void run() {
+        find(path);
+    }
+
+    private void find(String path) {
+        File[] listFiles = new File(path).listFiles();
+        for (File file : listFiles) {
+            if (file.isFile()) {
+                size += file.length();
+                count += 1;
+            }
+            if (file.isDirectory()) {
+                find(file.getPath());
+            }
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public long getSize() {
+        return size;
+    }
+}
